2b769425f1c68cb3073fbee274ebad1d6da44071
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-11-24 17:27:00 +0600

    homework_6

--- src/app/api/router.py
+++ src/app/api/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-
+from src.app.api.task.routes import router as task_router
 
 router = APIRouter(prefix='/api')
-task_router = APIRouter(prefix='/task')
+
 router.include_router(task_router)
 
 
